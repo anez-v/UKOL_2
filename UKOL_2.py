1fc779ce3df6e1a817fcfb37a64714b5e56890ce
Anežka Votrubová <votrubova.anezka@gmail.com>
2024-04-13 22:26:10 +0200

    ukol 2

---
+++ UKOL_2.py
@@ -0,0 +1,59 @@
+import requests
+
+#hledani podle ičo
+ico = input("Zadejte IČO:")
+adresa = f"https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/{ico}"
+response = requests.get(url=adresa)
+data = response.json()
+
+obchodni_jmeno = data["obchodniJmeno"]
+adresa_sidla = data["sidlo"]["textovaAdresa"]
+
+print(obchodni_jmeno,"\n", adresa_sidla)
+
+
+#hledani podle jmena nebo jeho casti
+#bonus - právní forma
+
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+data = '{"kodCiselniku": "PravniForma", "zdrojCiselniku": "res"}'
+res = requests.post("https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ciselniky-nazevniky/vyhledat", headers=headers, data=data)
+odpoved = res.json()
+polozky_ciselniku = odpoved["ciselniky"][0]["polozkyCiselniku"]
+
+
+def find_legal_form(kod, polozky_ciselniku):
+    kod = kod
+    for polozka in polozky_ciselniku:
+        kod_nazvu = polozka["kod"]
+        nazev = polozka["nazev"][0]["nazev"]
+        if kod == kod_nazvu:
+            return nazev
+    raise ValueError("Neplatný kód")
+
+
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json",
+}
+jmeno = input("Zadejte jméno nebo jeho část:")
+
+data = {"obchodniJmeno": jmeno}
+res = requests.post("https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat", headers=headers, json=data)
+odpoved_a = res.json()
+
+pocet_celkem = odpoved_a["pocetCelkem"]
+print(f"Nalezeno subjektů: {pocet_celkem}")
+
+
+ekon_subjekty = odpoved_a["ekonomickeSubjekty"]
+for subjekt in ekon_subjekty:
+    kod = subjekt['pravniForma']
+    pravni_forma = find_legal_form(kod, polozky_ciselniku)
+    print(f"{subjekt['obchodniJmeno']}, {subjekt['ico']}, {pravni_forma}")
+
+
